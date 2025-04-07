772010e763fb34a7065203732f15cd73d09c60db
Elicarlos Valentim <elicarlosalexandre89@gmail.com>
2025-04-07 12:24:24 -0300

    Add files via upload

--- Portfolio-eli2/Nike/scripts.js
+++ Portfolio-eli2/Nike/scripts.js
@@ -1,19 +1,66 @@
 let body = document.querySelector("body");
+let titulo = document.getElementById("titulo");
+let descricao = document.getElementById("descricao");
+let preco = document.getElementById("preco");
 let tenis = document.querySelector(".imagem-tenis");
 
-function mudarVisual(cor, imagem) {
-  // Adiciona a classe para aplicar o efeito
+const modelos = [
+  {
+    nome: "Nike Air Zoom",
+    descricao:
+      "Uma passada elástica para qualquer corrida, com mais conforto nas áreas sensíveis do seu pé.",
+    preco: "R$ 799,00",
+    imagem: "img/nike1.png",
+    corFundo: "#ccee66",
+  },
+  {
+    nome: "Nike Rosa Style",
+    descricao:
+      "Design moderno, conforto absoluto. Ideal para o dia a dia com um toque de estilo.",
+    preco: "R$ 849,00",
+    imagem: "img/nike3.png",
+    corFundo: "#ff9eb5",
+  },
+  {
+    nome: "Nike Blue Speed",
+    descricao:
+      "Alta performance para corridas intensas. Leveza e resistência em cada passo.",
+    preco: "R$ 899,00",
+    imagem: "img/nike2.png",
+    corFundo: "#58cced",
+  },
+];
+
+let indexAtual = 0;
+
+function atualizarVisual() {
+  const modelo = modelos[indexAtual];
+
   tenis.classList.add("troca-efeito");
+  body.style.background = modelo.corFundo;
+  
+  setInterval(() => {
+   proximoModelo();
+ }, 5000); // troca a cada 5 segundos
+ 
 
-  // Muda a cor de fundo
-  body.style.background = cor;
-
-  // Aguarda um tempinho antes de trocar a imagem
   setTimeout(() => {
-    tenis.src = imagem;
-
-    // Remove a classe de efeito para voltar ao normal
+    titulo.textContent = modelo.nome;
+    descricao.textContent = modelo.descricao;
+    preco.textContent = modelo.preco;
+    tenis.src = modelo.imagem;
     tenis.classList.remove("troca-efeito");
-  }, 300); // tempo em milissegundos (0.3 segundos)
+  }, 300);
 }
+
+function proximoModelo() {
+  indexAtual = (indexAtual + 1) % modelos.length;
+  atualizarVisual();
+}
+
+function voltarModelo() {
+  indexAtual = (indexAtual - 1 + modelos.length) % modelos.length;
+  atualizarVisual();
+}
+
 
